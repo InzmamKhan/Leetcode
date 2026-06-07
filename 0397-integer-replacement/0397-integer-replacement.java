9d5a58dd9f0303d40a2437bbfe481a60dc5175e2
Inzmam Khan <khaninzmam59@gmail.com>
2026-06-07 19:28:11 +0530

    Time: 0 ms (100%), Space: 42 MB (76.48%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int integerReplacement(int n) {
+        long num = n; 
+        int count = 0;
+        
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            }
+            else {
+                if (num % 4 == 3 && num != 3) {
+                    num += 1;
+                }
+                else {
+                    num -= 1;
+                }
+            }
+            count++;
+        }
+        
+        return count;
+    }
+}
