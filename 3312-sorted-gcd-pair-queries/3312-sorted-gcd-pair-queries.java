a5b8384be2a1f7c4750ec089ed6288f5237c5a9d
Inzmam Khan <khaninzmam59@gmail.com>
2026-07-18 00:12:31 +0530

    Time: 28 ms (96.88%), Space: 102.5 MB (46.88%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int maxVal = 0;
+        for (int num : nums) {
+            if (num > maxVal) {
+                maxVal = num;
+            }
+        }
+
+        int[] count = new int[maxVal + 1];
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        long[] gcdCount = new long[maxVal + 1];
+        for (int i = maxVal; i >= 1; i--) {
+            long totalMultiples = 0;
+            for (int j = i; j <= maxVal; j += i) {
+                totalMultiples += count[j];
+            }
+            
+            long totalPairs = (totalMultiples * (totalMultiples - 1)) / 2;
+            
+            for (int j = 2 * i; j <= maxVal; j += i) {
+                totalPairs -= gcdCount[j];
+            }
+            gcdCount[i] = totalPairs;
+        }
+
+        long[] prefixSums = new long[maxVal + 1];
+        for (int i = 1; i <= maxVal; i++) {
+            prefixSums[i] = prefixSums[i - 1] + gcdCount[i];
+        }
+
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            long target = queries[i] + 1;
+            int low = 1, high = maxVal;
+            int res = maxVal;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (prefixSums[mid] >= target) {
+                    res = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            answer[i] = res;
+        }
+
+        return answer;
+    }
+}
